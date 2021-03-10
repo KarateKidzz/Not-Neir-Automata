fedf40f443f347bf7fa2698af1664a8bc45c336b
James Kelly <jameskelly21699@gmail.com>
2021-03-10 14:01:31 +0000

    Start Implementing Attacking

---
+++ Unity/Not-Nier-Automata/Assets/_Project/Scripts/Pawns/Character.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : Pawn
+{
+    [Header("Character")]
+
+    [Header("Movement")]
+    public float turnSpeed = 10f;
+
+    [Header("Animation")]
+    public Animator animator;
+}
--- Unity/Not-Nier-Automata/Assets/_Project/Scripts/Pawns/Pawn.cs
+++ Unity/Not-Nier-Automata/Assets/_Project/Scripts/Pawns/Pawn.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Pawn : MonoBehaviour
 {
+    [Header("Pawn")]
+
     protected Controller owningController;
 
     [SerializeField]
