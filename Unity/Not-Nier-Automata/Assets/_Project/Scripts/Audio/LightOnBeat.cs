88ff98c148f95b655fbc50f78294529f577d40aa
James Kelly <jameskelly21699@gmail.com>
2021-03-21 20:48:37 +0000

    Add Beat Light to Pod

---
+++ Unity/Not-Nier-Automata/Assets/_Project/Scripts/Audio/LightOnBeat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class LightOnBeat : MonoBehaviour
+{
+    public Light controllingLight;
+
+    public float turnOffTime = 0.2f;
+
+    public float onIntensity = 1f;
+
+    public float offIntensity = 0f;
+
+    private void Start()
+    {
+        BeatCallbacks.OnBeatChange += OnBeat;
+    }
+
+    private void OnDestroy()
+    {
+        BeatCallbacks.OnBeatChange -= OnBeat;
+    }
+
+    void OnBeat(FMOD.Studio.EventInstance instance, FMOD.Studio.TIMELINE_BEAT_PROPERTIES beat)
+    {
+        controllingLight.intensity = onIntensity;
+
+        DOTween.To(() => controllingLight.intensity, value => controllingLight.intensity = value, offIntensity, turnOffTime);
+    }
+}
