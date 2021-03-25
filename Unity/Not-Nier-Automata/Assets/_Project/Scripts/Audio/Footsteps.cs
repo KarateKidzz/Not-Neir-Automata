a4b993664fd6e5a753aae155dd778da64bc1e77b
PlasticJazz <79145979+PlasticJazz@users.noreply.github.com>
2021-03-25 10:55:33 +0000

    Script Footsteps

---
+++ Unity/Not-Nier-Automata/Assets/_Project/Scripts/Audio/Footsteps.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using FMODUnity;
+
+public class Footsteps : MonoBehaviour
+{
+    [EventRef]
+    public string footstepEvent;
+    public void PlayFootstep()
+    {
+        RuntimeManager.PlayOneShotAttached(footstepEvent, gameObject);
+    }
+}
