43fbce81c82fe10bb538fe8ed85a39baafd5b74d
James Kelly <jameskelly21699@gmail.com>
2021-04-17 22:36:25 +0100

    Finish First Full Version of Intro Cinematic

--- Unity/Not-Nier-Automata/Assets/_Project/Scripts/GameModes/Level01GameMode.cs
+++ Unity/Not-Nier-Automata/Assets/_Project/Scripts/GameModes/Level01GameMode.cs
@@ -2,22 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using FMODUnity;
+using FMOD.Studio;
 
 public class Level01GameMode : GameMode
 {
     public PlayableDirector introDirector;
 
+    public GameObject[] introObjects;
+
+    [EventRef]
+    public string musicEvent;
+
+    [EventRef]
+    public string snapshotEvent;
+
+    EventInstance musicInstance;
+
+    EventInstance snapshotInstance;
+
     public override void StartGameMode()
     {
         base.StartGameMode();
 
+        introDirector.played += StartPlaying;
         introDirector.stopped += FinishedPlaying;
 
         introDirector.Play();
     }
 
+    void StartPlaying(PlayableDirector director)
+    {
+        //musicInstance = RuntimeManager.CreateInstance(musicEvent);
+        //musicInstance.start();
+
+        //snapshotInstance = RuntimeManager.CreateInstance(snapshotEvent);
+        //snapshotInstance.start();
+    }
+
     void FinishedPlaying(PlayableDirector director)
     {
-        ScriptExecution.BeginPlay();   
+        //musicInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        //musicInstance.release();
+
+        //snapshotInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        //snapshotInstance.release();
+
+        ScriptExecution.BeginPlay();
+
+        for (int i = 0; i < introObjects.Length; i++)
+        {
+            Destroy(introObjects[i]);
+        }
     }
 }
