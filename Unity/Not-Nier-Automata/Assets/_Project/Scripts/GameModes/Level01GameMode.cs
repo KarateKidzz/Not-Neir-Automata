97b816f32b2e342198cb0d5cdb2e1496e41ea6b2
James Kelly <jameskelly21699@gmail.com>
2021-04-17 21:03:29 +0100

    First Intro Cinematic

--- Unity/Not-Nier-Automata/Assets/_Project/Scripts/GameModes/Level01GameMode.cs
+++ Unity/Not-Nier-Automata/Assets/_Project/Scripts/GameModes/Level01GameMode.cs
@@ -1,8 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class Level01GameMode : GameMode
 {
+    public PlayableDirector introDirector;
 
+    public override void StartGameMode()
+    {
+        base.StartGameMode();
+
+        introDirector.stopped += FinishedPlaying;
+
+        introDirector.Play();
+    }
+
+    void FinishedPlaying(PlayableDirector director)
+    {
+        ScriptExecution.BeginPlay();   
+    }
 }
