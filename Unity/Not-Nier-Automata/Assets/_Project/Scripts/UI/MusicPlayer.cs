b7e05cfe4ff2def8f202e29b28407d2bad9dfca5
PlasticJazz <79145979+PlasticJazz@users.noreply.github.com>
2021-04-27 14:55:25 +0100

    Expanded Market

---
+++ Unity/Not-Nier-Automata/Assets/_Project/Scripts/UI/MusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using FMODUnity;
+using FMOD.Studio;
+
+public class MusicPlayer : MonoBehaviour
+
+{
+    [EventRef]
+    public string[] eventList;
+    
+    private 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
