ebeb6fb563a8b3030bbb6681b2c23fb45a92066e
James Kelly <jameskelly21699@gmail.com>
2021-05-06 20:10:05 +0100

    Remove Debugs

--- Unity/Not-Nier-Automata/Assets/_Project/Scripts/LevelTransitions/LevelTransitionOnTrigger.cs
+++ Unity/Not-Nier-Automata/Assets/_Project/Scripts/LevelTransitions/LevelTransitionOnTrigger.cs
@@ -8,11 +8,9 @@ public class LevelTransitionOnTrigger : Actor
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("ON TRIGGER");
         Debug.Log(other.tag);
         if (other.CompareTag("Player") || (other.attachedRigidbody && other.attachedRigidbody.CompareTag("Player")))
         {
-            Debug.Log("IS PLAYER");
             GameManager.Instance.LevelLoader.LoadScene(levelToLoad);
         }
     }
