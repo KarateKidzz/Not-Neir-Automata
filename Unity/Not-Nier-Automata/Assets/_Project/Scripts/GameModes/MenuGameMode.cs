f329780645e2908b3f8c2baa741f5d6021114ad4
James Kelly <jameskelly21699@gmail.com>
2021-05-09 15:22:19 +0100

    Finish Making Menu Pretty

--- Unity/Not-Nier-Automata/Assets/_Project/Scripts/GameModes/MenuGameMode.cs
+++ Unity/Not-Nier-Automata/Assets/_Project/Scripts/GameModes/MenuGameMode.cs
@@ -2,7 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuGameMode : GameMode
+public class MenuGameMode : GameMode, IBeginPlay
 {
+    public string podID;
 
+    public void BeginPlay()
+    {
+        UniqueAsset pod = AssetIDs.Instance.GetFirstInstanceOfID(podID);
+
+        if (pod)
+        {
+            pod.gameObject.transform.rotation = Quaternion.Euler(0, -136.9f, 0);
+        }
+    }
 }
--- Unity/Not-Nier-Automata/Assets/_Project/Scripts/Management/GameMode.cs
+++ Unity/Not-Nier-Automata/Assets/_Project/Scripts/Management/GameMode.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMode : MonoBehaviour
+public class GameMode : Actor
 {
     protected static bool quitting;
 
